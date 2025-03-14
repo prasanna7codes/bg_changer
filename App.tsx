dbe15a294b2f5ee39f066fadf5b9bc785e2f68a1
prasanna7codes <prasannalit098@gmail.com>
2025-03-14 22:27:31 +0530

    trying the colors

--- App.tsx
+++ App.tsx
@@ -21,30 +21,12 @@ function App(): React.JSX.Element {
 
 
   return (
-   
-   <View>
-    <Text>hi</Text>
-   </View>
+    <StatusBar backgroundColor={"#000000"}/>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+ 
 });
 
 export default App;
